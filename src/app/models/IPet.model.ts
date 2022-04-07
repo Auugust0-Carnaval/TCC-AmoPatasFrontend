0e64c23bb8af77665fa25287cf3bb892e7eeba72
carloskauan <carloskauan190@gmail.com>
2022-04-07 13:17:23 -0300

    subtituindo class por interface, e colocando propriedades pet

--- src/app/models/IPet.model.ts
+++ src/app/models/IPet.model.ts
@@ -1,4 +1,12 @@
-export class IPet{
-    //Propriedades
+export interface IPet{
+  //Propriedades
+  id: number;
+  nome: string;
+  descricao: string;
+  dataPublicacao: string;
+  dataAdocao: string;
+  cdAnimal: string;
+  ufAnimal: string;
+  idade: number;
 }
 
