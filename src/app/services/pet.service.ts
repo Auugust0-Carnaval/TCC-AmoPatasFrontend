d78f246566f9ab44d83c497c7f3201cc7d3ad912
carloskauan <carloskauan190@gmail.com>
2022-04-10 19:51:36 -0300

    Adicionando URL do backend, instanciando um variável http

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -1,15 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PetService {
-  //variável que conterá a URL de onde ele irá buscar as informações do nosso produto.
-  private URL: string = ""
-
-  constructor() { }
+  //variável que conterá a URL de onde ele irá buscar as informações do nossos pets;
+  private URL: string = "http://localhost:5186/Pets"
+  // criação da variável que irá fazer a auto instanciação do meu objeto HttpClient;
+  constructor(private http: HttpClient) {}
   // métodos
   buscarTodos(){
-    // desenvolvimento
+    // método que iremos utilizar para buscar os dados dos nossos pets;
+    return this.http.get(this.URL);
   }
 }
