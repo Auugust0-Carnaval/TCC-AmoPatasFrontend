50982d43e777a00ec720fb78fb3fe4006f6d0b90
Sianguii <sisiestudos@gmail.com>
2022-04-13 16:26:51 -0300

    Mudando tipagem das propriedades number para any

--- src/app/models/Pet.model.ts
+++ src/app/models/Pet.model.ts
@@ -1,30 +1,19 @@
 export interface Pet{
   //Propriedades mudadas
-  IdAnimal?: number;
+  IdAnimal?: any;
   nmAnimal: string;
   dsAnimal: string;
   dtPublicacao: Date;
   dtAdocao: Date;
   cdAnimal: string;
   UfAnimal: string;
-  Idade: number;
-  IdPorte: number;
-  IdPessoa: number;
-  IdRaca: number;
-  IdCategoria: number;
-  IdSituacao: number;
-  IdTutor: number; // isso int = number /isso mesmo akakka vedda
-  IdSexo : number;
-  IdDoador: number; //<3 protin ta certin testar ela hihhih ve se funfa 8 ta certin
-
-// vou conseguir be tela o pai
+  Idade: any;
+  IdPorte: any;
+  IdPessoa: any;
+  IdRaca: any;
+  IdCategoria: any;
+  IdSituacao: any // isso int = number /isso mesmo akakka vedda
+  IdSexo : any;
+  IdDoador: any;
+  IdTutor: any; //<3 protin ta certin testar ela hihhih ve se funfa 8 ta certin
 }
-
-
-
-// ta erradoo tme que ta igual da API sim sim tudin pode deixarr muuitas
-
-//vamos tentar?? vams tive uma ide iahih ok vamos fazer a comparacção
-// belezinha ja vi um erro heheheheh eu tmb ro vendo mas nao to tebdi certexa mas vms fazer funfmatCardAvatar isso ai be
-
-
