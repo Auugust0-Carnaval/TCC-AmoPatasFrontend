693a4d27c1f888f249e729b32fe3319b90024e1d
Sianguii <sisiestudos@gmail.com>
2022-04-16 00:09:16 -0300

    Criando metodos de busca e registo para o consumo da API

--- src/app/components/pets/cadastrar-pets/cadastrar-pets.component.ts
+++ src/app/components/pets/cadastrar-pets/cadastrar-pets.component.ts
@@ -1,6 +1,22 @@
+import { CategoriaService } from './../../../services/categoria.service';
+import { SexoService } from './../../../services/sexo.service';
+import { PorteService } from './../../../services/porte.service';
+import { PessoaService } from './../../../services/pessoa.service';
+import { Byte } from '@angular/compiler/src/util';
+import { Pessoa } from './../../../models/Pessoa.model';
+import { Categoria } from './../../../models/Categoria.model';
+import { Situacao } from './../../../models/Situacao.model';
+import { Porte } from './../../../models/Porte.model';
+import { Sexo } from './../../../models/Sexo.model';
+import { Raca } from './../../../models/Raca.model';
+import { RacaService } from './../../../services/raca.service';
+import Swal from 'sweetalert2';
 import { Pet } from './../../../models/Pet.model';
 import { PetService } from './../../../services/pet.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { now } from 'jquery';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-pets',
@@ -9,22 +25,174 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarPetsComponent implements OnInit {
 
+
+
   listaPets: Pet[] = []; // aqui é um array conjutno de informações da classe pets pode deixar bêe não fao diea
+  listaRaca: Raca[] = []; // é onde vem as informações do banco e fica armazenadas nesse array
+  listaPorte: Porte[] = [];
+  listaCategoria: Categoria[] = [];
+  listaSexo: Sexo[] = [];
+  listaPessoa: Pessoa[] = [];
 
-  constructor(private petService: PetService ) { }
 
-  ngOnInit(): void {
-    this.carregarPets();
+  pet: Pet = {
+    nmAnimal: '',//
+    dsAnimal: '',//
+    dtPublicacao: new Date(),//
+    dtAdocao: new Date(),//
+    cdAnimal: '',//
+    UfAnimal: '',//'
+    Idade: 0,//
+    IdPorte: 0, //
+    IdPessoa: 0,
+    IdRaca: 0, //
+    IdCategoria: 0,//
+    IdSituacao: 0,//
+    IdSexo : 0,//
+    IdDoador: 0, //
+    IdTutor: 0//
+  };
+
+  raca : Raca = {
+    dsRaca : '',
+    IdCategoria: 0,
+    IdRaca : 0
+  };
+
+  sexo : Sexo = {
+    IdSexo : 0,
+    dsSexo : ""
+  };
+
+  porte : Porte = {
+    IdPorte : 0,
+    dsPorte : ""
+  };
+
+  categoria : Categoria = {
+    IdCategoria : 0,
+    dsCategoria : ""
+  };
+
+  pessoa : Pessoa = {
+    nmPessoa : "",
+    Email : "",
+    Telefone : "",
+    Password : "",
+    DataNascimento : new Date(),
+    Descricao : "",
+    RedeSocial : "",
+    FotoPerfil : 0
   }
 
 
-  carregarPets() : void
+
+
+  // nmRaca : string = ""; // o que é isso kakakak queria testar aglo
+
+  constructor(private PetService: PetService, private router: Router, private RacaService: RacaService, private PessoaService : PessoaService,  private PorteService : PorteService, private SexoService: SexoService , private CategoriaService: CategoriaService) { }
+
+  ngOnInit(): void {
+  }
+
+  /*carregarPets() : void
+  {
+    this.PetService.buscarTodos().subscribe(returno => [
+      this.listaPets = returno
+    ])
+  };*/
+
+
+  salvarPet(): void {
+    this.PetService.cadastrar(this.pet).subscribe(retorno => {
+      this.pet = retorno;
+      this.exibirSucesso();
+    });
+  }
+  exibirSucesso()
+  {
+     //`${this.pet.nmAnimal} foi cadastrado com sucesso. ID: ${this.pet.IdAnimal}`,
+    Swal.fire(`${this.pet.nmAnimal} foi cadastrado com sucesso. ID: ${this.pet.IdAnimal}`);
+  }
+
+  carregarRaca() : void
+{
+ this.RacaService.buscarTodos().subscribe(ra => [
+   this.listaRaca = ra
+ ])
+};
+
+  salvaRaca() : void{
+
+    this.RacaService.cadastrar(this.raca).subscribe(retorno => {
+      this.raca = retorno;
+   });
+ }
+
+
+ carregarPorte() : void
+ {
+   this.PorteService.buscarTodos().subscribe(por => [
+     this.listaPorte = por
+   ])
+ };
+
+ salvaPorte() : void{
+
+  this.PorteService.cadastrar(this.porte).subscribe(retorno => {
+    this.porte = retorno;
+ });
+}
+
+
+carregarSexo() : void
+{
+ this.SexoService.buscarTodos().subscribe(sex => [
+   this.listaSexo = sex
+ ])
+};
+
+salvaSexo() : void{
+
+  this.SexoService.cadastrar(this.sexo).subscribe(retorno => {
+    this.sexo = retorno;
+ });
+}
+
+carregarCategoria() : void
+{
+ this.CategoriaService.buscarTodos().subscribe(cat => [
+   this.listaCategoria = cat
+ ])
+};
+
+salvaCategoria() : void{
+
+  this.CategoriaService.cadastrar(this.categoria).subscribe(retorno => {
+    this.categoria = retorno;
+ });
+}
+
+carregarPessoa() : void
+{
+ this.PessoaService.buscarTodos().subscribe(pessoa => [
+   this.listaPessoa = pessoa
+ ])
+};
+
+salvaPessoa() : void{
+
+  this.PessoaService.cadastrar(this.pessoa).subscribe(retorno => {
+    this.pessoa = retorno;
+ });
+}
+
+}
+  /*carregarPets() : void
   {
 
     this.petService.buscarTodos().subscribe(returno => [
       this.listaPets = returno
     ])
-  };
+  };*/ //esse aqui ele tava buscando aquele metodo la buscartodos que é de pet
 
-
-}
