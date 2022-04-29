4dd6489cce8cd0f673e7599a963350de403cdf5f
Sianguii <sisiestudos@gmail.com>
2022-04-29 14:58:45 -0300

    Adicionando metodo de Buscar Pets

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,5 @@
+import { Pet } from './../../models/Pet.model';
+import { PetService } from './../../services/pet.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  ListaPet : Pet[] = []
+
+  constructor(private petService : PetService) { }
 
   ngOnInit(): void {
   }
 
+
+  QueryAll() : void{
+    this.petService.buscarTodos().subscribe(retorno =>
+      this.ListaPet = retorno
+    )
+  }
 }
