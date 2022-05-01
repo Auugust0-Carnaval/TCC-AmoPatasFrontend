ccc8f1485ed5f753b1a07237a8f4e96b6861b8e7
Sianguii <sisiestudos@gmail.com>
2022-05-01 01:36:11 -0300

    Tentando usar JavaScript no angular :(

--- src/app/models/Pet.model.ts
+++ src/app/models/Pet.model.ts
@@ -1,19 +1,22 @@
 export interface Pet{
   //Propriedades mudadas
-  IdAnimal?: number;
-  nmAnimal: string;
-  dsAnimal: string;
-  dtPublicacao: Date;
-  dtAdocao: Date;
-  cdAnimal: string;
-  UfAnimal: string;
-  Idade: number;
-  IdPorte: number;
-  IdPessoa: number;
-  IdRaca: number;
-  IdCategoria: number;
-  IdSituacao: number; // isso int = number /isso mesmo akakka vedda
-  IdSexo : number;
-  IdDoador: number;
-  IdTutor: number;//<3 protin ta certin testar ela hihhih ve se funfa 8 ta certin
+  name: string;
+  age: any;
+  breed: string;
+  imagem: string;
+  descricao: string;
+  uf: string;
+  sexo: string;
+  porte: string;
+  situacao: string;
+
+
+
+//   IdCategoria: number;
+//   IdSituacao: number; // isso int = number /isso mesmo akakka vedda
+//   IdSexo : number;
+//   IdDoador: number;
+//   IdTutor: number;//<3 protin ta certin testar ela hihhih ve se funfa 8 ta certin
+
+
 }
