06de33a8352f77434983188f83e76b4684fba0c9
Sianguii <sisiestudos@gmail.com>
2022-05-05 17:09:43 -0300

    Tirando propriedade de imagem {Deve ser adicionado depois}

---
+++ src/app/models/User.model.ts
@@ -0,0 +1,11 @@
+export interface User{
+  //Propriedades mudadas
+  name: string;
+  email: any;
+  senha: string;
+  // imagem: string;
+  telefone: string;
+  descricao: string;
+  data_nascimento: string;
+  rede_social: string;
+}
