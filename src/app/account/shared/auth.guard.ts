91289deba8cdd264b39b570b95984e61a1a84798
Sianguii <sisiestudos@gmail.com>
2022-05-29 18:38:29 -0300

    Navegacao caso o usuario esteja 'autenticado'

--- src/app/account/shared/auth.guard.ts
+++ src/app/account/shared/auth.guard.ts
@@ -1,15 +1,32 @@
+import { AuthService } from './../../components/login/auth.service';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate,ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthGuard implements CanActivate {
+
+    constructor( private authService : AuthService,
+                 private router: Router // forcando voltar ora tela de login caso nao esteja autentivcad
+    ){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+
+    ): Observable<boolean> | boolean{
+      if(this.authService.usuarioEstaAutenticado()){
+        return true; // se o usuario tivcer autenticado retornar true e ele pode entrar
+      }
+      else{
+        this.router.navigate(['/login']); // indicando a rota qe é pra voltar
+          return false;
+      }
+    }
+
   }
-  
-}
+
+
