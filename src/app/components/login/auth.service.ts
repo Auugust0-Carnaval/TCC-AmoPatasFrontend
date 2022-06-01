f8c45d4f8ac20c084d5fed22c42fbe114f158a25
Sianguii <sisiestudos@gmail.com>
2022-05-31 21:09:36 -0300

    Fazendo vereficacao do token e autenticando usuario

--- src/app/components/login/auth.service.ts
+++ src/app/components/login/auth.service.ts
@@ -2,12 +2,16 @@ import { UserService } from './../../services/user.service';
 import { User } from './../../models/User.model';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
     private usuarioAutenticado: boolean = false;
+
+    private jwt : any;
+
 
 
     mostrarMenuEmitter = new EventEmitter<boolean>(); // isso é pra nao mostrar o =menu pro usuario no caso a nav bar
@@ -17,9 +21,18 @@ export class AuthService {
   constructor(private router: Router, private userService : UserService) { }
 
 
-  fazerLogin(user : User){
-    if(user.email === 'usuario@email.com' &&
-    user.senha === '12345')
+  fazerLogin(user : any){
+
+
+    this.jwt = user;
+
+    console.log(this.jwt);
+
+
+
+
+
+    if(this.jwt)
     {
       this.usuarioAutenticado = true;
       this.mostrarMenuEmitter.emit(true);
@@ -29,9 +42,15 @@ export class AuthService {
       this.usuarioAutenticado = false;
       this.mostrarMenuEmitter.emit(false);
 
+      // Swal.fire({
+      //   title : `<span class = "text-sucess"><strong class="text-danger">${user.email.toUpperCase()}</strong> NÃO POSSUI CADASTRO</span>`,
+      //   timer: 5000,
+      // showConfirmButton: false
+
+      // });
+
       console.log('nao funfo')
     }
-    console.log(user);
   }
 
   usuarioEstaAutenticado(){
