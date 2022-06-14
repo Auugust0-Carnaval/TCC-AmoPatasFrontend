e9fa27febab241f0fbf6fb605d30e8674759be83
Sianguii <sisiestudos@gmail.com>
2022-06-14 15:00:46 -0300

    Adicionando metodo de deletar-pet

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,5 +1,8 @@
+import { PetService } from 'src/app/services/pet.service';
 import { UserDataService } from './../../services/user-data.service';
 import { Component, Input, OnInit } from '@angular/core';
+import { Pet } from 'src/app/models/Pet.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-perfil',
@@ -11,10 +14,52 @@ export class PerfilComponent implements OnInit {
 
   public user: any;
 
-  constructor(private userdata: UserDataService) { }
+  Pets: Pet[] = []
+
+  public gfg = true;
+
+  constructor(private userdata: UserDataService, private petservice: PetService ) { }
 
   ngOnInit(): void {
     this.user = this.userdata.getData();
-    console.log(this.user);
+    this.GetPeUser();
   }
+
+  GetPeUser(){
+    this.petservice.buscarId(this.user.id).subscribe(retorno =>{
+      this.Pets = retorno
+    })
+  }
+
+  deletePet(petid: any){
+    Swal.fire({
+      icon:'error',
+      title: `<strong><span class="uk-text-warning">DESEJA REALMENTE DELETAR O <span class="uk-text-danger">${petid.name.toUpperCase()}</span>?</span></strong>`,
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: true,
+      confirmButtonText:
+        '<i class="thumbs up outline icon"></i> SIM!',
+      cancelButtonText:
+        '<i class="thumbs down outline icon" aria-hidden="true"></i> NAO!'
+    }).then((result)=>{
+
+      if(result.isConfirmed){
+        this.petservice.DeletePet(petid).subscribe(dados =>{console.log(dados); this.GetPeUser()});
+        Swal.fire({
+          imageUrl : 'assets/img/notfound.jpg',
+          imageWidth: 300,
+          imageHeight: 240,
+          title: `<span class = "text-success"><span class="text-danger">${petid.name.toUpperCase()}</span> APAGADO COM SUCESSO</span>`,
+          timer: 3000,
+          showCancelButton: false,
+          showConfirmButton :false
+        })
+
+
+      }
+    })
+  }
+
+
 }
