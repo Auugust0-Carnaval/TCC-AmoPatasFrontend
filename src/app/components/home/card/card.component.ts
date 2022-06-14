6e1a32ad0d823431b5d4f50f496845f0b19de409
Sianguii <sisiestudos@gmail.com>
2022-06-14 14:59:57 -0300

    Tirando metodo de deletar-pet

--- src/app/components/home/card/card.component.ts
+++ src/app/components/home/card/card.component.ts
@@ -42,35 +42,5 @@ export class CardComponent implements OnInit {
       this.usuario = retorno);
 
   }
-
-  deletePet(petid: any){
-    Swal.fire({
-      icon:'error',
-      title: `<strong><span class="uk-text-warning">DESEJA REALMENTE DELETAR O <span class="uk-text-danger">${petid.name.toUpperCase()}</span>?</span></strong>`,
-      showCloseButton: true,
-      showCancelButton: true,
-      focusConfirm: true,
-      confirmButtonText:
-        '<i class="thumbs up outline icon"></i> SIM!',
-      cancelButtonText:
-        '<i class="thumbs down outline icon" aria-hidden="true"></i> NAO!'
-    }).then((result)=>{
-
-      if(result.isConfirmed){
-        this.petService.DeletePet(petid).subscribe(dados =>{console.log(dados); this.BuscarPets()});
-        Swal.fire({
-          imageUrl : 'assets/img/notfound.jpg',
-          imageWidth: 300,
-          imageHeight: 240,
-          title: `<span class = "text-success"><span class="text-danger">${petid.name.toUpperCase()}</span> APAGADO COM SUCESSO</span>`,
-          timer: 3000,
-          showCancelButton: false,
-          showConfirmButton :false
-        })
-
-
-      }
-    })
-  }
 }
 
