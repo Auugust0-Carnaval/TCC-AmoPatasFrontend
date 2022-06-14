971c3abf9f2bae56c339e5d183336c4cbbedd680
Sianguii <sisiestudos@gmail.com>
2022-06-14 13:23:13 -0300

    Setando array do usuario autenticado em UserDataService

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,9 @@
+import { UserDataService } from './../../services/user-data.service';
 import { AuthService } from './auth.service';
 import { UserService } from './../../services/user.service';
 import { HttpClient } from '@angular/common/http';
 import { User } from './../../models/User.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 
 import { HttpInterceptor } from '@angular/common/http';
 
@@ -17,23 +18,26 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+ public usuario: any;
 
-
- public user : User = {
-  email : '',
-  senha : ''
-}
+  public user : User = {
+    email : '',
+    senha : ''
+  }
 
  public token : any;
 
- public usuario: any;
 
-  constructor(private authService : AuthService, private userService : UserService) { }
+
+ constructor(private authService : AuthService, private userService : UserService, private userdata: UserDataService) { }
 
   ngOnInit(): void {
+    console.log(this.userdata.getData());
   }
 
   LoginUser(){
+
+    this.userdata.setData(this.usuario);
 
     this.userService.loginUsuario(this.user).subscribe(retorno =>{
       this.token = retorno;
@@ -44,7 +48,7 @@ export class LoginComponent implements OnInit {
     })
     this.authService.fazerLogin(this.token);
 
-
+    console.log(this.usuario);
 
     Swal.fire({
       title: `<img class="ui small centered circular image" src="http://localhost:3333/files/${this.usuario.imagem}">
