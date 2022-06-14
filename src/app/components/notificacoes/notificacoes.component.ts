3472e5b90af227317f39415dd7028ceec471874f
Sianguii <sisiestudos@gmail.com>
2022-06-14 13:25:11 -0300

    Consumindo retorno do usuario autenticado do UserDataService

--- src/app/components/notificacoes/notificacoes.component.ts
+++ src/app/components/notificacoes/notificacoes.component.ts
@@ -1,3 +1,4 @@
+import { UserDataService } from './../../services/user-data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notificacoes.component.css']
 })
 export class NotificacoesComponent implements OnInit {
+  public user: any;
 
-  constructor() { }
+  constructor(private userdata: UserDataService) { }
 
   ngOnInit(): void {
+    this.user = this.userdata.getData();
+    console.log(this.user);
   }
 
 }
