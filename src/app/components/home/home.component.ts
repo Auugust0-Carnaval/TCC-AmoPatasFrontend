99a85533e1b0cbb32084e52a92fc005253f5f033
Augusto <augustooliveixa@gmail.com>
2022-06-28 00:55:40 -0300

    Adicionando toast de solicitacao: true

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
+import { UserDataService } from './../../services/user-data.service';
+import { SolicitacaoService } from './../../services/solicitacao.service';
 import { Pet } from './../../models/Pet.model';
 import { PetService } from './../../services/pet.service';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -9,27 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  Pets : Pet[] = []
+  public usuario: any
 
-  constructor(private petService : PetService) { }
+  public mensagem: any
+
+  constructor(private petService : PetService, private solicitacaoservice: SolicitacaoService, private userData: UserDataService) { }
 
   ngOnInit(): void {
-    this.BuscarPets(); // Ao iniciar a aplicação (COMPONENT) vai acionar o metodo de busca
-    // this.teste();
+    this.usuario = this.userData.getData();
+    this.SucessoSolicitacao();
   }
 
 
-  BuscarPets() : void{
-    this.petService.buscarTodos().subscribe(retorno =>
-      this.Pets = retorno
-    )
-  }
+  async SucessoSolicitacao(){
+    this.solicitacaoservice.UserIdSolic(this.usuario.id).subscribe(retorno =>{
+      this.mensagem = retorno
+    })
 
+   await this.delay(5000);
 
+    let soliciMensagem = this.mensagem.solicita;
 
-  teste(){
-    for(var i = 2; i> 0; i++){
-      console.log('fodase');
+    if(soliciMensagem[0].aprovado == true){
+      Swal.fire({
+        position:'top',
+        title:`<br><i class="thumbtack icon" style="color:blue; font-size:55px"></i>',`,
+        html: `<strong><span class ="text-warning">SOLICITAÇÃO FOI ACEITA PELO(A)</span><span class="text-danger"> ${this.mensagem.solicita[0].name_dono.toUpperCase()}</span></strong>`,
+        showConfirmButton: false,
+        timer:3000
+      })
+
     }
   }
+
+  private delay(ms: number): Promise<boolean> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(true);
+      }, ms);
+    });
+  }
+
+
 }
